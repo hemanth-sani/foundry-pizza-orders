2421866896dccaca230e6e90a6693cd0f4757d29
Hemanth Sani <00d13a3c-2173-4a27-a8da-08c891ac7247>
2025-04-13 03:19:21 +0000

    Autogenerated commit message at Apr 12, 2025 at 23:34

--- src/hooks/usePizzaMenu.ts
+++ src/hooks/usePizzaMenu.ts
@@ -1,11 +1,11 @@
 import useSWR from "swr";
 import client from "../client";
-import { HemanthPizza } from "@pizza-ordering-application/sdk";
+import { HemanthBestSellingPizza } from "@pizza-ordering-application/sdk";
 import { Osdk } from "@osdk/client";
 
 export default function usePizzaMenu() {
-  const { data, error, isLoading } = useSWR<Osdk.Instance<HemanthPizza>[]>("pizzaMenu", async () => {
-    const page = await client(HemanthPizza).fetchPage({ $pageSize: 50 });
+  const { data, error, isLoading } = useSWR<Osdk.Instance<HemanthBestSellingPizza>[]>("pizzaMenu", async () => {
+    const page = await client(HemanthBestSellingPizza).fetchPage({ $pageSize: 50 });
     return page.data;
   });
 
